434673d04f67c8794f54e8a3c883fe3593a2388a
Nirmal <66936373+itsnirmal@users.noreply.github.com>
2023-02-28 12:29:31 -0600

    Added projects in Projects page

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function projects() {
   return (
-    <div className='App-header'><h1>Projects</h1></div>
+    <div className='App-header'>
+    
+      <h1>Projects</h1>
+      <a href='https://github.com/itsnirmal/Face-Detection-Using-python-opencv' target= "_blank" rel="noreferrer noopener">Face-Detection Using Python and OpenCV</a>
+      <a href='https://github.com/itsnirmal/Face-Recognition-with-Python-and-OpenCV' target= "_blank" rel="noreferrer noopener">Face-Recognition Using Python and OpenCV</a>
+      <a href='https://github.com/itsnirmal/Student-Database-Web-APP' target= "_blank" rel="noreferrer noopener">Student Database Web Application Using Spring Boot and Maven</a>
+    </div>
+    
   )
 }
 
