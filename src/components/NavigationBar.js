0082b922f274b16784f7126e3259d6fe9ebe0bfa
Nirmal <66936373+itsnirmal@users.noreply.github.com>
2023-03-06 22:45:33 -0600

    Made changes to make navigation bar responsive

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,11 +5,13 @@ import './navbar.css';
 import { Spin as Hamburger} from 'hamburger-react';
 
 const NavigationBar = () => {
-  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+  
+    const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
+    const toggleMenu = () => {
+    setMobileMenuVisible(!mobileMenuVisible);
+  
+}
 
   return (
     <nav className="navbar">
@@ -17,21 +19,26 @@ const NavigationBar = () => {
         <Link to="/" className="navbar-logo">
           itsnirmal
         </Link>
-         <div className={`navbar-menu ${isOpen ? 'active' : ''}`}>
-          <ul className='navbar-list'>
+         <div className="navbar-menu" >
+          
+        </div>
+        <div className={mobileMenuVisible ? "mobile-menu active" : "mobile-menu"}>
+        <ul className='navbar-list'>
             <li className='navbar-item'>
-              <Link to='/' className='navbar-link' onClick={toggleMenu}>
+              <Link to='/' className='navbar-link' onClick= {toggleMenu}>
                 Home
               </Link>
             </li>
             <li className='navbar-item'>
-              <Link to='/projects' className='navbar-link' onClick={toggleMenu}>
+              <Link to='/projects' className='navbar-link' onClick= {toggleMenu}>
                 Projects
               </Link>
             </li>
           </ul>
-        </div>
-        <Hamburger onToggle={toggleMenu}  rounded />
+      </div>
+      <div className='hamburger-icon'>
+        <Hamburger onToggle={toggleMenu} rounded />
+      </div>
       </div>
     </nav>
   );
